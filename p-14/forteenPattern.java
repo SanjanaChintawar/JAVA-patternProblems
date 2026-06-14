89af1fe6a832c71b1d075aa41130ee6aedadc42f
Sanjana Chintawar <sanjanachintawar007@gmail.com>
2026-06-15 02:23:12 +0530

    14th commit

---
+++ p-14/forteenPattern.java
@@ -0,0 +1,14 @@
+public class forteenPattern {
+    public static void main(String[] args){
+        
+        for(int i = 0; i < 5; i++){
+            char alpha = 'A';
+            for(int j = 0; j <= i; j++){
+                System.out.print(" " + alpha + " ");
+                alpha++;
+            }
+            System.out.println();
+            
+        }
+    }
+}
